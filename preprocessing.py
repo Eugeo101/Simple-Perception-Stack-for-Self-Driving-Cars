70138eda159e3bdd1b9e08935d8889c7d2ae1132
Ahmed Ayman <88598980+Eugeo101@users.noreply.github.com>
2022-04-24 19:40:13 +0200

    input image and output is ROI img using polylines

--- preprocessing.py
+++ preprocessing.py
@@ -4,3 +4,15 @@ import shared_data
     To access Shared data:
         shared_data.orig_frame
 """
+def plot_ROI(src, frame = None, plot = False):
+    #input img
+    #output get image with ROI
+    
+    if plot == True:
+        #cv2.polylines(image, [pts], isClosed, color, thickness)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        roi_img = cv2.polylines(frame, [src], True, (217, 14, 23), 3)
+        plt.imshow(roi_img)
+        
+    else:
+        return
