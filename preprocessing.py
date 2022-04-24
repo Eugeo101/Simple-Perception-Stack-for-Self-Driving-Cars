c204f25704ced864d803ddfeb2c1a20e9cfba20c
Ahmed Ayman <88598980+Eugeo101@users.noreply.github.com>
2022-04-24 19:41:20 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,13 +1,11 @@
 import shared_data
 
-"""
-    To access Shared data:
-        shared_data.orig_frame
-"""
 def plot_ROI(src, frame = None, plot = False):
     #input img
     #output get image with ROI
     
+    if frame == None:
+        frame = shared_data.orig_frame
     if plot == True:
         #cv2.polylines(image, [pts], isClosed, color, thickness)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
