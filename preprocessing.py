107fbc079b36ff525018e8e9639ea0bc9f228bbc
Ahmed Ayman <88598980+Eugeo101@users.noreply.github.com>
2022-04-24 19:57:33 +0200

    attach global variables with the function ROI

--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,17 @@
 import shared_data
 
-def plot_ROI(src, frame = None, plot = False):
+def plot_ROI(frame = None, plot = False):
     #input img
     #output get image with ROI
     
     if frame == None:
-        frame = shared_data.orig_frame
+        frame = shared_data.orig_frame.copy()
     if plot == True:
         #cv2.polylines(image, [pts], isClosed, color, thickness)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        roi_img = cv2.polylines(frame, [src], True, (217, 14, 23), 3)
+        roi_img = cv2.polylines(frame, np.int32([shared_data.roi_points]), True, (217, 14, 23), 3)
         plt.imshow(roi_img)
-        
+        plt.title("ROI Image")
+        plt.axis("off")
     else:
         return
